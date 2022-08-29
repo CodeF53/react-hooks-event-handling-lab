9a1dee337443736cb129009e3d08b328f5ca28d2
ChaseWelsh <fseusb@gmail.com>
2022-08-29 15:47:29 -0600

    commit tree fdcbe8bf3d75a479a02636c97588f5b52b483a7b

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
+const log = log => console.log(log);
+
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    return <button onFocus={()=>{log("Good!")}} onBlur={()=>{log("Hey! Eyes on me!")}}>Eyes on me</button>
+}
+
+export default EyesOnMe;
